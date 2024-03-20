3ab215dc5a3db3f39ef0f2f643c1ca730b89f071
Tanishka Marrott <tanishka.marrott@gmail.com>
2024-03-20 23:31:25 +0530

    Updated

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
+module "vpc" {
+  source                  = "./modules/vpc"
+  tags                    = { "Name" = "aws-eks-cluster" }
+  instance_tenancy        = "default"
+  vpc_cidr                = "10.0.0.0/16"
+  access_ip               = "0.0.0.0/0"  #Will adjust 
+  public_sn_count         = 2
+  public_cidrs            = ["10.0.1.0/24", "10.0.2.0/24"]  # CIDRs for public subnets.
+  map_public_ip_on_launch = true
+  rt_route_cidr_block     = "0.0.0.0/0"
+  azs                     = ["ap-northeast-1a", "ap-northeast-1b"]  # Multi-AZ for higher availability.
+}
+
 module "eks" {
   source                  = "./modules/eks"
   aws_public_subnet       = module.vpc.aws_public_subnet
@@ -5,24 +18,17 @@ module "eks" {
   cluster_name            = "module-eks-${random_string.suffix.result}"
   endpoint_public_access  = true
   endpoint_private_access = false
-  public_access_cidrs     = ["0.0.0.0/0"]
+  public_access_cidrs     = ["52.69.121.167/32", "115.96.77.190/32"]  # Restricting Kubernetes API server access - my Ip and Jenkins Severs IP
   node_group_name         = "aws-eks-cluster"
-  scaling_desired_size    = 1
-  scaling_max_size        = 1
+  scaling_desired_size    = 2
+  scaling_max_size        = 4
   scaling_min_size        = 1
   instance_types          = ["t3.small"]
-  key_pair                = "ssh-sree"
-}
-
-module "vpc" {
-  source                  = "./modules/vpc"
-  tags                    = "aws-eks-cluster"
-  instance_tenancy        = "default"
-  vpc_cidr                = "10.0.0.0/16"
-  access_ip               = "0.0.0.0/0"
-  public_sn_count         = 2
-  public_cidrs            = ["10.0.1.0/24", "10.0.2.0/24"]
-  map_public_ip_on_launch = true
-  rt_route_cidr_block     = "0.0.0.0/0"
-
+  key_pair                = "tanishka-tokyo-ssh-key"
+  tags = {
+    "Environment" = "production"
+    "Project"     = "EKS Cluster"
+  }
+  # Enabled Logging
+  cluster_logging_types   = ["api", "audit", "authenticator"]
 }
