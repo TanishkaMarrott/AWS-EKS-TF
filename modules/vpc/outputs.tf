c60a4077093370cce6f536a8bd74540d6492809a
Tanishka Marrott <tanishka.marrott@gmail.com>
2024-03-21 22:57:54 +0530

    Update output.tf

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,8 +1,11 @@
-###### vpc/outputs.tf 
+###### vpc/outputs.tf
+
+# List of IDs for the public subnets created within the AWS EKS cluster's VPC
 output "aws_public_subnet" {
   value = aws_subnet.public_aws-eks-cluster_subnet.*.id
 }
 
+# The ID of the VPC created for the AWS EKS cluster
 output "vpc_id" {
   value = aws_vpc.aws-eks-cluster.id
 }
