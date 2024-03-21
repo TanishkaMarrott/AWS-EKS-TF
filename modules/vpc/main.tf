a62ebdd63a2d2e476fd852dd7899181ff0b90383
Tanishka Marrott <tanishka.marrott@gmail.com>
2024-03-21 16:10:20 +0530

    Update main.tf - VPC

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,4 +1,4 @@
-#######modules/vpc/main.tf
+# Defining the VPC for the AWS EKS cluster
 resource "aws_vpc" "aws-eks-cluster" {
   cidr_block       = var.vpc_cidr 
   instance_tenancy = var.instance_tenancy
@@ -7,6 +7,7 @@ resource "aws_vpc" "aws-eks-cluster" {
   }
 }
 
+# Creating an Internet Gateway for the VPC
 resource "aws_internet_gateway" "aws-eks-cluster_gw" {
   vpc_id = aws_vpc.aws-eks-cluster.id
 
@@ -15,15 +16,17 @@ resource "aws_internet_gateway" "aws-eks-cluster_gw" {
   }
 }
 
+# List of available Availability Zones for deploying the subnet in ap-northeast-1
 data "aws_availability_zones" "available" {
 }
 
-
+# Randomly shuffling the list of available Availability Zones to select a subset for deploying subnets
 resource "random_shuffle" "az_list" {
   input        = data.aws_availability_zones.available.names
   result_count = 2
 }
 
+# Creating public subnets in the VPC in the selected Availability Zones
 resource "aws_subnet" "public_aws-eks-cluster_subnet" {
   count                   = var.public_sn_count
   vpc_id                  = aws_vpc.aws-eks-cluster.id
@@ -35,7 +38,7 @@ resource "aws_subnet" "public_aws-eks-cluster_subnet" {
   }
 }
 
-
+# Associating the default route table with a route to IG for outbound internet access
 resource "aws_default_route_table" "internal_aws-eks-cluster_default" {
   default_route_table_id = aws_vpc.aws-eks-cluster.default_route_table_id
 
@@ -48,6 +51,7 @@ resource "aws_default_route_table" "internal_aws-eks-cluster_default" {
   }
 }
 
+# Associating the created public subnets with the default route table for internet connectivity
 resource "aws_route_table_association" "default" {
   count          = var.public_sn_count
   subnet_id      = aws_subnet.public_aws-eks-cluster_subnet[count.index].id
