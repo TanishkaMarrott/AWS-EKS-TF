98f583b0c866b6d9abf7e9b757aa8ef79645bd6f
Tanishka Marrott <tanishka.marrott@gmail.com>
2024-03-21 13:20:54 +0530

    Kubernetes Integration and S3 Backend Configuration

--- provider.tf
+++ provider.tf
@@ -1,11 +1,10 @@
+# Terraform version and providers needed for the project
 terraform {
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.15.0"
     }
-
     random = {
       source  = "hashicorp/random"
       version = "3.1.0"
@@ -16,31 +15,37 @@ terraform {
     }
   }
 
-
-backend "s3" {
-    bucket = "s3-bakend-tfstate"
+  # Configure the backend to store Terraform state in my S3 bucket
+  backend "s3" {
+    bucket = "s3-backend-tfstate-tanishka"
     key    = "Terraform-eks/terraform.tfstate"
-    region = "us-east-1"
+    region = "ap-northeast-1"
   }
 }
 
+# Cluster ID for the EKS cluster
 data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_id
 }
+
+# Authentication data for the EKS cluster
 data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_id
 }
+
+# Configure the Kubernetes provider
 provider "kubernetes" {
   cluster_ca_certificate = base64decode(module.eks.kubeconfig-certificate-authority-data)
   host                   = data.aws_eks_cluster.cluster.endpoint
   token                  = data.aws_eks_cluster_auth.cluster.token
-
 }
+
 
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-northeast-1"
 }
 
+# To use as a suffix for resources needing unique names
 resource "random_string" "suffix" {
   length  = 5
   special = false
