1ed41a832e7abc35da88741ac75e378a32e2e847
Tanishka Marrott <tanishka.marrott@gmail.com>
2024-03-24 00:26:08 +0530

    Enhancements to vpc/main.tf and vpc/variables.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,21 +1,20 @@
 # Defining the VPC for the AWS EKS cluster
 resource "aws_vpc" "aws-eks-cluster" {
-  cidr_block       = var.vpc_cidr 
-  instance_tenancy = var.instance_tenancy
-  enable_dns_support = true
+  cidr_block           = var.vpc_cidr
+  instance_tenancy     = var.instance_tenancy
+  enable_dns_support   = true
   enable_dns_hostnames = true
-  tags = var.tags
+  tags                 = var.tags
 }
 
 # Creating an Internet Gateway for the VPC
 resource "aws_internet_gateway" "aws-eks-cluster_gw" {
   vpc_id = aws_vpc.aws-eks-cluster.id
-  tags = var.tags
+  tags   = var.tags
 }
 
 # List of available Availability Zones for deploying the subnet
-data "aws_availability_zones" "available" {
-}
+data "aws_availability_zones" "available" {}
 
 # Randomly shuffling the list of available Availability Zones
 resource "random_shuffle" "az_list" {
@@ -30,24 +29,51 @@ resource "aws_subnet" "public_aws-eks-cluster_subnet" {
   cidr_block              = element(var.public_cidrs, count.index)
   availability_zone       = element(random_shuffle.az_list.result, count.index)
   map_public_ip_on_launch = true
-  tags = var.tags
-  
+  tags                    = var.tags
 }
 
 # Creating private subnets in the VPC
 resource "aws_subnet" "private_aws-eks-cluster_subnet" {
-  count             = length(var.private_cidrs)
-  vpc_id            = aws_vpc.aws-eks-cluster.id
-  cidr_block        = element(var.private_cidrs, count.index)
-  availability_zone = element(random_shuffle.az_list.result, count.index)
+  count                   = length(var.private_cidrs)
+  vpc_id                  = aws_vpc.aws-eks-cluster.id
+  cidr_block              = element(var.private_cidrs, count.index)
+  availability_zone       = element(random_shuffle.az_list.result, count.index)
   map_public_ip_on_launch = false
+  tags                    = var.tags
+}
+
+# Creating Elastic IP for NAT Gateway
+resource "aws_eip" "nat" {
+ 
+}
+
+# Creating a NAT Gateway in the first public subnet
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_aws-eks-cluster_subnet[0].id
+  tags          = var.tags
+}
+
+# Creating a route table for private subnets to route traffic through the NAT Gateway
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.aws-eks-cluster.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw.id
+  }
   tags = var.tags
 }
 
-# Associating the default route table with the Internet Gateway
+# Associating private subnets with the route table
+resource "aws_route_table_association" "private" {
+  count          = length(var.private_cidrs)
+  subnet_id      = aws_subnet.private_aws-eks-cluster_subnet[count.index].id
+  route_table_id = aws_route_table.private.id
+}
+
+# Associating the default route table with the Internet Gateway for public subnets
 resource "aws_default_route_table" "internal_aws-eks-cluster_default" {
   default_route_table_id = aws_vpc.aws-eks-cluster.default_route_table_id
-
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.aws-eks-cluster_gw.id
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,34 +1,65 @@
 # The CIDR block for the VPC that will be created
-variable "vpc_cidr" {}
+variable "vpc_cidr" {
+  description = "The CIDR block for the AWS VPC."
+  type        = string
+  default     = "10.0.0.0/16"
+}
 
-# The IP address that will be allowed access to the VPC
-variable "access_ip" {}
+# The IP address that will be allowed access to the VPC (specifically for the NAT Gateway and Security Group rules)
+variable "access_ip" {
+  description = "The IP address range that will be allowed access to the VPC."
+  type        = string
+  default     = "0.0.0.0/0"
+}
 
 # The number of public subnets to create within the VPC
-variable "public_sn_count" {}
+variable "public_sn_count" {
+  description = "The number of public subnets to create within the VPC."
+  type        = number
+  default     = 2
+}
 
 # A list of CIDR blocks for the public subnets within the VPC
 variable "public_cidrs" {
-  type = list(any)
-}
-
-variable "instance_tenancy" {}
-
-variable "tags" {
-  description = "A map of tags to add to all resources"
-  type        = map(string)
-  default     = {}
-}
-
-variable "private_cidrs" {
-  description = "CIDR blocks for private subnets"
+  description = "A list of CIDR blocks for the public subnets within the VPC."
   type        = list(string)
-  default     = ["10.0.3.0/24", "10.0.4.0/24"] 
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
+# The instance tenancy of the VPC
+variable "instance_tenancy" {
+  description = "The instance tenancy option for the VPC."
+  type        = string
+  default     = "default"
+}
+
+# Tags to be applied to all resources created
+variable "tags" {
+  description = "A map of tags to add to all resources."
+  type        = map(string)
+  default     = {
+    "Project" = "AWS EKS Setup"
+    "Environment" = "Development"
+  }
+}
+
+# A list of CIDR blocks for the private subnets within the VPC
+variable "private_cidrs" {
+  description = "A list of CIDR blocks for the private subnets within the VPC."
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
 
 # Whether instances launched in public subnets should automatically be assigned a public IP address.
-variable "map_public_ip_on_launch" {}
+variable "map_public_ip_on_launch" {
+  description = "Determines whether instances launched in public subnets should be assigned a public IP address."
+  type        = bool
+  default     = true
+}
 
 # The CIDR block for the route in the VPC's route table that directs traffic to the internet gateway
-variable "rt_route_cidr_block" {}
+variable "rt_route_cidr_block" {
+  description = "The CIDR block for the route in the VPC's route table that directs traffic to the internet gateway."
+  type        = string
+  default     = "0.0.0.0/0"
+}
